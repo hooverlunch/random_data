9d653e09d9cac902555f8d65c49fb6c9134cdce2
James Harker <james@njtechnologies.co.uk>
2012-07-26 22:44:27 +0100

    Postcode regions

--- lib/random_data/locations.rb
+++ lib/random_data/locations.rb
@@ -50,14 +50,15 @@ module RandomData
 
     # Returns a string providing something in the general form of a UK post code.  Like the zip codes, this might
     # not actually be valid. Doesn't cover London whose codes are like "SE1".
-
+      
+    @@postcode_region = %w(AB AL B BA BB BD BH BL BN BR BS BT CA CB CF CH CM CO CR CT CV CW DA DD DE DG DH DL DN DT DY E EC EH EN EX FK FY G GL GU HA HD HG HP HR HS HU HX IG IP IV KA KT KW KY L LA LD LE LL LN LS LU M ME MK ML N NE NG NN NP NR NW OL OX PA PE PH PL PO PR RG RH RM S SA SE SG SK SL SM SN SO SP SR SS ST SW SY TA TD TF TN TQ TR TS TW UB W WA WC WD WF WN WR WS WV YO ZE)
+    
     def uk_post_code
       number_1  = rand(100).to_s
       number_2  = rand(100).to_s
-      start_letters = ("AA".."ZZ").to_a.rand
       end_letters = ("AA".."ZZ").to_a.rand
 
-      return "#{start_letters}#{number_1} #{number_2}#{end_letters}"
+      return "#{@@postcode_region.rand}#{number_1} #{number_2}#{end_letters}"
     end
 
     # from technoweenie: http://svn.techno-weenie.net/projects/plugins/us_states/lib/us_states.rb
