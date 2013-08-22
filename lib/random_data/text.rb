d7c8831554c885443a16c94437f14d120af05561
Thomas Smyth <tom@tomsmyth.ca>
2013-08-22 13:06:20 -0400

    added some text methods

--- lib/random_data/text.rb
+++ lib/random_data/text.rb
@@ -27,25 +27,13 @@ module RandomData
      "Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat",
      "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
      "Excepteur sint occaecat cupidatat non proident  sunt in culpa qui officia deserunt mollit anim id est laborum",
-     "There's a voice that keeps on calling me",
-     "Down the road that's where I'll always be",
-     "Every stop I make I make a new friend; Can't stay for long just turn around and I'm gone again",
-     "Maybe tomorrow I'll want to settle down - until tomorrow I'll just keep moving on",
-     "Hong Kong Phooey number one super guy, Hong Kong Phooey quicker than the human eye",
-     "He's got style, a groovy style, and a car that just won't stop",
-     "Hey there where ya goin, not exactly knowin'",
-     "Who says you have to call just one place home?",
-     "He's going everywhere, B.J. McKay and his best friend Bear",
-     "He just keeps on movin' and ladies keep improvin'",
-     "Every day is better than the last, with new dreams and better scenes and best of all I don't pay property tax",
-     "Rolling  down to Dallas - who is providin' my palace?",
-     "Off to New Orleans or who knows where",
-     "Soaring through all the galaxies in search of Earth flying in to the night",
-     "Ulysses, fighting evil and tyranny with all his power and with all of his might",
-     "No-one else can do the things you do, like a bolt of thunder from the blue",
-     "Always fighting all the evil forces bringing peace and justice to all",
-     "I've gotten burned over Cheryl Tiegs and blown up for Raquel Welch, but when I end up in the hay it's only hay, hey hey",
-     "I might jump an open drawbridge or Tarzan from a vine, beause I'm the unknown stuntman that makes Eastwood look so fine"] 
+     "Ed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae",
+     "Ab illo inventore veritatis et quasi architecto beatae vitae dicta",
+     "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur",
+     "Magni dolores eos qui ratione voluptatem sequi nesciunt",
+     "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius",
+     "Modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem"
+    ]
 
     # Returns a given number of paragraphs delimited by two newlines (defaults to two paragraphs), using a small pool of generic sentences.
     # >> Random.paragraphs  
@@ -53,18 +41,34 @@ module RandomData
     # "I might jump an open drawbridge or Tarzan from a vine, beause I'm the unknown stuntman that makes Eastwood look so fine.\n\n \Always fighting all the evil forces bringing peace and justice to all. \n\n"
      
    def paragraphs(num = 2)
-     text = ''
+      paras = []
  
      num.times do 
+       sents = []
        (rand(5)+1).times do
-         text += @@sentences.rand + '. '
+         sents << @@sentences.rand
        end
-       text += "\n\n"
+       paras << sents.join('. ')
      end
  
-     return text
- 
+     return params.join("\n\n")
    end
+
+    def words(size = 1)
+      paragraphs.split(" ")[0,size].join(" ").gsub(/[,;?\.'"\-\n]/, "").downcase
+    end
+    
+    def phrase(bound = 5)
+      words(rand(bound)).capitalize
+    end
+    
+    def question
+      sentence + "?"
+    end
+    
+    def sentence
+      @@sentences.rand
+    end
 
  end
 end
